0d85fbcc450962092f18782875b0199836f2feff
Rostislav Shtanko <rostik404@174.128-25.206.45.5.in-addr.arpa>
2016-06-30 12:19:57 +0300

    little changes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log(eval("2 + 3 * 2"));
 var https = require("https");
 var stdin = process.openStdin();
 
@@ -24,22 +25,28 @@ function validateName(name) {
 function requestToGithub(name){
   var request = https.request(options, function(response){
     var body = '';
+
     response.on("data", function(chunk){
-        body += chunk.toString('utf8');
+        readData(body, chunk);
     });
-
-    response.on("end", function(){
-        var result = JSON.parse(body);
-
-        if(result.type != undefined) {
-          console.log("type: " + result.type + "\n public repos: "
-           + result.public_repos + "\n followers: " + result.followers);
-        } else {
-          console.log("This user doesn't exist");
-        }
-    });
+    response.on("end", showResults, body);
   });
   request.end();
+}
+
+function readData(body, chunk) {
+  body += chunk.toString('utf8');
+}
+
+function showResults(result) {
+  result = JSON.parse(result);
+
+  if(result.type != undefined) {
+    console.log("type: " + result.type + "\n public repos: "
+     + result.public_repos + "\n followers: " + result.followers);
+  } else {
+    console.log("This user doesn't exist");
+  }
 }
 
 module.exports.validateName = validateName;
